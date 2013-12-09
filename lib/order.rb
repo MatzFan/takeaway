a5e0f67d614113b2f86e3cfadf6e1c712b40a62a
Bruce Steedman <bruce.steedman@gmail.com>
2013-12-09 16:57:04 +0000

    Added methods to check order value against arg provided by customer

--- lib/order.rb
+++ lib/order.rb
@@ -2,9 +2,26 @@ class Order
 
   attr_reader :dishes, :total
 
-  def initialize(total = 0, dishes = {})
+  def initialize(total = 0, dishes)
     @total = total
     @dishes = dishes
+    check_total
+  end
+
+  def check_total
+    raise ArgumentError, "Order total is #{calc_total}, not #{total}" if calc_total != total
+  end
+
+  def calc_total
+    all_dishes = {pizza: 5.5, curry: 7.5}
+    total = 0
+    dishes.each { |dish, quantity| total += all_dishes[dish] * quantity}
+    total
   end
 
 end # of class
+
+
+dishes = {pizza: 5.5, curry: 7.5}
+o = Order.new(18.5, {pizza: 2, curry: 1})
+p o.calc_total
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,15 +3,24 @@ require 'order'
 describe Order do
 
   let(:takeaway) { double('takeaway') }
-  let(:order) { Order.new(13, {pizza: 2, curry: 3}) }
+  let(:valid_order) { Order.new(13, {pizza: 2}, {curry: 3}) }
+  let(:invalid_order) { Order.new(12, {pizza: 2}, {curry: 3}) }
 
   context '#initialize' do
-    it 'a customer should be able to place an order with dishes specified' do
-      order.dishes.should == ({pizza: 2, curry: 3})
+    it 'a customer should be able to place an order with several dishes specified' do
+      valid_order.dishes.should == ([{pizza: 2}, {curry: 3}])
     end
 
     it 'a customer should be able to place an order with order total specified' do
-      order.total.should == (13)
+      valid_order.total.should == (13)
+    end
+
+    it 'should calculate its total value' do
+      lambda { invalid_order }.should raise_error(IncorrectTotal, 'Order total is £13, not £12')
+    end
+
+    xit 'if a customer provides incorrect total an error should be reaised' do
+      lambda { invalid_order }.should raise_error(IncorrectTotal, 'Order total is £13, not £12')
     end
   end
 
